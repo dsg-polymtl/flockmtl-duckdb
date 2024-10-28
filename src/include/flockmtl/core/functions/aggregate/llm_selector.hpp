7ef5020ba179b03754d325085136afe3648bb8f5
Anas Dorbani <anasdorbani@gmail.com>
2024-10-28 22:08:11 +0100

    add the llm_selector to be used in llm_max_and llm_min

---
+++ src/include/flockmtl/core/functions/aggregate/llm_selector.hpp
@@ -0,0 +1,42 @@
+#include "flockmtl/common.hpp"
+#include "flockmtl/core/model_manager/model_manager.hpp"
+#include "flockmtl/core/model_manager/tiktoken.hpp"
+#include "flockmtl/core/module.hpp"
+
+#include <inja/inja.hpp>
+#include <nlohmann/json.hpp>
+#include <string>
+#include <vector>
+
+namespace flockmtl {
+namespace core {
+
+struct LlmSelectorState {
+public:
+    std::vector<nlohmann::json> value;
+
+    void Initialize();
+    void Update(const nlohmann::json &input);
+    void Combine(const LlmSelectorState &source);
+};
+
+class LlmSelector {
+public:
+    std::string model;
+    int model_context_size;
+    std::string user_prompt;
+    std::string llm_selector_template;
+    int available_tokens;
+
+    LlmSelector(std::string &model, int model_context_size, std::string &user_prompt,
+                std::string &llm_selector_template);
+
+    nlohmann::json GetElement(const nlohmann::json &tuples);
+    nlohmann::json LlmSelectorCall(nlohmann::json &tuples);
+
+private:
+    int calculateFixedTokens() const;
+};
+
+} // namespace core
+} // namespace flockmtl
