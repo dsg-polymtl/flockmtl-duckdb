cd3f873db7af20261c632bc1ad2c79b860b0d2c3
Sunny Yasser <sunnyyasser@gmail.com>
2024-10-28 22:20:58 -0400

    Add AzureModelManager for making calls to Azure AI, seperate Session and OpenAI headers

--- src/core/model_manager/model_manager.cpp
+++ src/core/model_manager/model_manager.cpp
@@ -1,21 +1,27 @@
 #include <flockmtl/common.hpp>
 #include <flockmtl/core/model_manager/model_manager.hpp>
 #include <flockmtl/core/model_manager/openai.hpp>
+#include <flockmtl/core/model_manager/azure.hpp>
 #include <flockmtl_extension.hpp>
+#include <memory>
 
 namespace flockmtl {
 namespace core {
 
-std::string ModelManager::OPENAI_API_KEY = "";
 static const std::unordered_set<std::string> supported_models = {"gpt-4o", "gpt-4o-mini"};
 static const std::unordered_set<std::string> supported_providers = {"openai", "azure"};
 static const std::unordered_set<std::string> supported_embedding_models = {"text-embedding-3-small", "text-embedding-3-large"};
 
-
 nlohmann::json ModelManager::OpenAICallComplete(const std::string &prompt, const std::string &model,
                                                 const nlohmann::json &settings, const bool json_response) {
 
-    openai::start(ModelManager::OPENAI_API_KEY);
+    // Get API key from the environment variable
+    const char *key = openai::OpenAI::get_openai_api_key();
+    if (!key) {
+        throw std::runtime_error("OPENAI_API_KEY environment variable is not set.");
+    } else {
+        openai::start(key);
+    }
 
     // check if settings is not empty and has max_tokens and temperature else make some default values
     auto max_tokens = 4000;
@@ -85,8 +91,84 @@ nlohmann::json ModelManager::OpenAICallComplete(const std::string &prompt, const
 
 nlohmann::json ModelManager::AzureCallComplete(const std::string &prompt, const std::string &model,
                                                 const nlohmann::json &settings, const bool json_response) {
-    nlohmann::json json_obj;
-    return json_obj;
+    // Get API key from the environment variable
+    const char *api_key = AzureModelManager::get_azure_api_key ();
+    if (!api_key) {
+        throw std::runtime_error("AZURE_API_KEY environment variable is not set.");
+    }
+
+    const char * resource_name = AzureModelManager::get_azure_resource_name();
+    if (!resource_name) {
+        throw std::runtime_error("AZURE_RESOURCE_NAME environment variable is not set.");
+    }
+
+    const char * api_version = AzureModelManager::get_azure_api_version ();
+    if (!api_version) {
+        throw std::runtime_error("AZURE_VERSION environment variable is not set.");
+    }
+
+    auto azure_model_manager_uptr = std::make_unique<AzureModelManager> (api_key, resource_name, model, api_version, false);
+
+    // check if settings is not empty and has max_tokens and temperature else make some default values
+    auto max_tokens = 4000;
+    auto temperature = 0.5;
+    if (!settings.empty()) {
+        for (auto &[key, value] : settings.items()) {
+            if (key == "max_tokens") {
+                max_tokens = std::stoi(static_cast<std::string>(value));
+            } else if (key == "temperature") {
+                temperature = std::stof(static_cast<std::string>(value));
+            } else if (key == "provider") {
+                ;
+            } else {
+                throw std::invalid_argument("Invalid setting key: " + key);
+            }
+        }
+    }
+
+    // Create a JSON request payload with the provided parameters
+    nlohmann::json request_payload = {{"model", model},
+                                      {"messages", {{{"role", "user"}, {"content", prompt}}}},
+                                      {"max_tokens", max_tokens},
+                                      {"temperature", temperature}};
+
+    // Conditionally add "response_format" if json_response is true
+    if (json_response) {
+        request_payload["response_format"] = {{"type", "json_object"}};
+    }
+
+    // Make a request to the Azure API
+    auto completion = azure_model_manager_uptr->CallComplete(request_payload);
+
+    // Check if the conversation was too long for the context window
+    if (completion["choices"][0]["finish_reason"] == "length") {
+        // Handle the error when the context window is too long
+        throw std::runtime_error(
+            "The response exceeded the context window length you can increase your max_tokens parameter.");
+        // Add error handling code here
+    }
+
+    // Check if the safety system refused the request
+    if (completion["choices"][0]["message"]["refusal"] != nullptr) {
+        // Handle refusal error
+        throw std::runtime_error("The request was refused due to OpenAI's safety system.{\"refusal\": \"" +
+                                 completion["choices"][0]["message"]["refusal"].get<std::string>() + "\"}");
+    }
+
+    // Check if the model's output included restricted content
+    if (completion["choices"][0]["finish_reason"] == "content_filter") {
+        // Handle content filtering
+        throw std::runtime_error("The content filter was triggered, resulting in incomplete JSON.");
+        // Add error handling code here
+    }
+
+    std::string content_str = completion["choices"][0]["message"]["content"];
+
+    if (json_response) {
+        return nlohmann::json::parse(content_str);
+    }
+
+    return content_str;
 }
 
 
@@ -105,7 +187,7 @@ nlohmann::json ModelManager::CallComplete(const std::string &prompt, const std::
 }
 
 nlohmann::json ModelManager::CallComplete(const std::string &prompt, const std::string &model,
-                                          const std::string &provider, const std::string &settings,
+                                          const std::string &provider, const nlohmann::json &settings,
                                           const bool json_response) {
 
     // Check if the provided model is in the list of supported models
@@ -116,7 +198,7 @@ nlohmann::json ModelManager::CallComplete(const std::string &prompt, const std::
     }
 
     // Check if the provider is in the list of supported provider
-    if (supported_providers.find(model) == supported_providers.end()) {
+    if (provider != "default" and supported_providers.find(provider) == supported_providers.end()) {
         throw std::invalid_argument("Provider '" + provider +
                                 "' is not supported. Please choose one from the supported list: "
                                 "openai/default, azure");
@@ -132,12 +214,11 @@ nlohmann::json ModelManager::CallComplete(const std::string &prompt, const std::
 
 nlohmann::json ModelManager::OpenAICallEmbedding(const std::string &input, const std::string &model) {
     // Get API key from the environment variable
-    const char *key = std::getenv("OPENAI_API_KEY");
+    const char *key = openai::OpenAI::get_openai_api_key();
     if (!key) {
         throw std::runtime_error("OPENAI_API_KEY environment variable is not set.");
     } else {
-        openai::start(); // Assume it uses the environment variable if no API
-                         // key is provided
+        openai::start(key);
     }
 
     // Create a JSON request payload with the provided parameters
@@ -164,8 +245,44 @@ nlohmann::json ModelManager::OpenAICallEmbedding(const std::string &input, const
 
 
 nlohmann::json ModelManager::AzureCallEmbedding(const std::string &input, const std::string &model) {
-    nlohmann::json json_obj;
-    return json_obj;
+    // Get API key from the environment variable
+    const char *api_key = AzureModelManager::get_azure_api_key ();
+    if (!api_key) {
+        throw std::runtime_error("AZURE_API_KEY environment variable is not set.");
+    }
+
+    const char * resource_name = AzureModelManager::get_azure_resource_name();
+    if (!resource_name) {
+        throw std::runtime_error("AZURE_RESOURCE_NAME environment variable is not set.");
+    }
+
+    const char * api_version = AzureModelManager::get_azure_api_version ();
+    if (!api_version) {
+        throw std::runtime_error("AZURE_VERSION environment variable is not set.");
+    }
+
+    auto azure_model_manager_uptr = std::make_unique<AzureModelManager> (api_key, resource_name, model, api_version, false);
+
+    // Create a JSON request payload with the provided parameters
+    nlohmann::json request_payload = {
+        {"model", model},
+        {"input", input},
+    };
+
+    // Make a request to the Azure API
+    auto completion = azure_model_manager_uptr->CallEmbedding(request_payload);
+
+    // Check if the conversation was too long for the context window
+    if (completion["choices"][0]["finish_reason"] == "length") {
+        // Handle the error when the context window is too long
+        throw std::runtime_error(
+            "The response exceeded the context window length you can increase your max_tokens parameter.");
+        // Add error handling code here
+    }
+
+    auto embedding = completion["data"][0]["embedding"];
+
+    return embedding;
 }
 
 
--- src/include/flockmtl/core/model_manager/model_manager.hpp
+++ src/include/flockmtl/core/model_manager/model_manager.hpp
@@ -8,13 +8,11 @@ namespace core {
 
 struct ModelManager {
 public:
-    static std::string OPENAI_API_KEY;
-
     static nlohmann::json CallComplete(const std::string &prompt, const std::string &model,
                                        const nlohmann::json &settings, const bool json_response = true);
 
     static nlohmann::json CallComplete(const std::string &prompt, const std::string &model, const std::string &provider,
-                                       const std::string &settings, const bool json_response = true);
+                                       const nlohmann::json &settings, const bool json_response = true);
 
     static nlohmann::json CallEmbedding(const std::string &input, const std::string &model);
     static nlohmann::json CallEmbedding(const std::string &input, const std::string &model, const std::string &provider);
